297065c3cc109990ad5bebc46df540bfd6f548f5
kks940104 <kks940104@gmail.com>
2025-01-13 16:20:08 +0900

    member 추가중

---
+++ src/main/java/org/koreait/global/entities/BaseEntity.java
@@ -0,0 +1,44 @@
+package org.koreait.global.entities;
+
+import lombok.Data;
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Data
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseEntity {
+
+    @CreatedDate
+    @Column(updatable = false)
+    @JsonFormat(pattern = "yyyy-MM-DD HH:mm:ss")
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(insertable = false)
+    @JsonFormat(pattern = "yyyy-MM-DD HH:mm:ss")
+    private LocalDateTime modifiedAt;
+
+    @JsonFormat(pattern = "yyyy-MM-DD HH:mm:ss")
+    private LocalDateTime deletedAt;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/main/java/org/koreait/member/services/MemberInfoService.java
@@ -0,0 +1,72 @@
+package org.koreait.member.services;
+
+import org.modelmapper.ModelMapper;
+import org.koreait.member.MemberInfo;
+import lombok.RequiredArgsConstructor;
+import org.koreait.member.entities.Member;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import org.springframework.stereotype.Service;
+import org.koreait.member.constants.Authority;
+import org.koreait.member.entities.Authorities;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.context.annotation.Lazy;
+import org.koreait.member.repositories.MemberRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import java.util.List;
+
+/**
+ * Spring Security는 UserDetailsService 구현해야함.
+ */
+@Lazy
+@Service
+@RequiredArgsConstructor
+public class MemberInfoService implements UserDetailsService {
+
+    private final MemberRepository memberRepository;
+    private final JPAQueryFactory queryFactory;
+    private final HttpServletRequest request;
+    private final ModelMapper modelMapper;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Member member = memberRepository.findByEmage(username).orElseThrow(() -> new UsernameNotFoundException(username));
+
+        List<Authorities> items = member.getAuthorities();
+        if (items == null) {
+            Authorities auth = new Authorities();
+            auth.setMember(member);
+            auth.setAuthority(Authority.USER);
+            items = List.of(auth);
+        }
+
+        List<SimpleGrantedAuthority> authorities = items.stream().map(a -> new SimpleGrantedAuthority(a.getAuthority().name())).toList();
+
+        // 추가 정보 처리.
+
+        addInfo(member);
+
+        return MemberInfo.builder()
+                .email(member.getEmail())
+                .password(member.getPassword())
+                .member(member)
+                .authorities(authorities)
+                .build();
+    }
+
+    public Member get(String email) {
+        MemberInfo memberInfo = (MemberInfo) loadUserByUsername(email);
+        return memberInfo.getMember();
+    }
+
+    /**
+     * 추가 정보 처리
+     * @param member
+     */
+    public void addInfo(Member member) {
+
+    }
+}
