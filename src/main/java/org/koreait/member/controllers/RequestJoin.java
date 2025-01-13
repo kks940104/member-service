2cdac606cf6fc0e88a202ee919382b5ab2f7b172
kks940104 <kks940104@gmail.com>
2025-01-13 16:36:48 +0900

    member 추가중

---
+++ src/main/java/org/koreait/member/controllers/RequestJoin.java
@@ -0,0 +1,33 @@
+package org.koreait.member.controllers;
+
+import lombok.Data;
+import jakarta.validation.constraints.*;
+
+import java.util.List;
+
+@Data
+public class RequestJoin {
+
+    @Email
+    @NotBlank
+    private String email; // 이메일
+
+    @NotBlank
+    private String name; // 회원명
+
+    @Size(min=8)
+    private String password; // 비밀번호
+
+    private String confirmPassword; // 비밀번호 확인
+
+    @AssertTrue
+    private boolean requiredTerms1; // 필수 약관 동의 여부
+
+    @AssertTrue
+    private boolean requiredTerms2;
+
+    @AssertTrue
+    private boolean requiredTerms3;
+
+    private List<String> optionalTerms; // 선택 약관 동의 여부 -> 선택약관은 어떤 약관인지를 구분할 수 있어야함.
+}
